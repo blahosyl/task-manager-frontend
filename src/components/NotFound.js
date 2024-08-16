da329a3a53423e647b5b1d9abe33d7a580ec0768
Sylvia Blaho <sylvia.blaho@gmail.com>
2024-08-16 13:37:33 +0000

    style Not Found page #130

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,17 +1,15 @@
 import React from 'react'
-import NoResults from "../assets/no-results.png"
 import styles from "../styles/NotFound.module.css"
-import Asset from './Asset'
 import { Link } from 'react-router-dom/cjs/react-router-dom.min';
 
 const NotFound = () => {
   return (
     <div className={styles.NotFound}>
-        <Asset
-            src={NoResults}
-            message={`Sorry, the page you're looking for doesn't exist`} 
-        />
-        <Link to="/">Click here to go back to the home page</Link>
+        <p>Sorry, the page you're looking for doesn't exist</p>
+        <Link 
+          className={styles.NotFoundLink}
+          to="/"
+        >Click here to go back to the home page</Link>
     </div>
   );
 };
